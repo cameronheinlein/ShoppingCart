add7fe6479f27243c65847ddf806438b33227332
cameronheinlein <76449880+cameronheinlein@users.noreply.github.com>
2021-06-11 17:44:47 -0400

    add react components

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import Products from "./components/products";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Products></Products>
+  );
+}
+
+export default App;
